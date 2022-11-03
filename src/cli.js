93642d0afa4fd6c705cfc67dfcc264cd166f9270
aspogorelova <anyapogorelova17@gmail.com>
2022-11-03 11:02:35 +0300

    fix: corrected order of 1-2 lines

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,11 @@
 import readlineSync from 'readline-sync';
 
+const askName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return `Hello, ${userName}`;
+};
+export default askName;
+
+/*
 export const userName = readlineSync.question('May I have your name? ');
+*/
