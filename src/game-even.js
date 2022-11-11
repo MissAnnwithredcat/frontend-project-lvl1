82827ab6b622dd03feaf4e9d3f37885f1ec674a3
aspogorelova <anyapogorelova17@gmail.com>
2022-11-11 16:32:31 +0300

    fix: corrected function

--- src/game-even.js
+++ src/game-even.js
@@ -13,7 +13,8 @@ const askEvenNumber = () => {
   console.log(`Hello, ${nameUser}`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  let result = '';
+  let result = 0;
+  let sum = 0;
 
   // ЛОГИКА ИГРЫ
   for (let i = 0; i < 3; i += 1) {
@@ -23,24 +24,23 @@ const askEvenNumber = () => {
     console.log(`Question: ${number}`);
     // Просим вписать ответ:
     const answerUser = readlineSync.question('Your answer: ');
-    // Проверяем ответ на правильность:
+    // Определяем правильный ответ:
     if (number % 2 === 0) {
-      if (answerUser === 'yes') {
-        result = 'true';
-        console.log('Correct!');
-      } else {
-        result = 'false';
-        console.log(`'${answerUser}' is wrong answer ;(. Correct answer was 'yes'.`);
-        break;
-      }
-    } else if (answerUser === 'no') {
-      result = 'true';
+      result = 'yes';
+    } else {
+      result = 'no';
+    }
+
+    // Выводим результат Игроку:
+    if (result === answerUser) {
+      sum += 1;
       console.log('Correct!');
     } else {
-      result = 'false';
-      return `'${answerUser}' is wrong answer ;(. Correct answer was 'no'.`;
+      console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${result}'.`);
+      break;
     }
   }
-  return (result === 'true') ? console.log(`Congratulations, ${nameUser}!`) : console.log(`Let's try again, ${nameUser}!`);
+
+  return (sum === 3) ? console.log(`Congratulations, ${nameUser}!`) : console.log(`Let's try again, ${nameUser}!`);
 };
 export default askEvenNumber;
