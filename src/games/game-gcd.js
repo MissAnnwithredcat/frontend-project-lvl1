b0bd37e2cbe0669e4bb4bb5a00ff8ffe63fcba9d
aspogorelova <anyapogorelova17@gmail.com>
2022-12-26 16:40:02 +0300

    fix: added error message, rename function

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,16 +1,23 @@
 import runGame from '../index.js';
 import getRandomNumber from '../utils.js';
 
-const getGreaterCommonDivider = (dividend, divider) => {
+const getGCD = (dividend, divider) => {
   let num1 = dividend;
   let num2 = divider;
+  if (num1 === 0 || num1 === 1) {
+    return Error(`${num1} is not a prime or a composite number. Please, try again.`);
+  }
+  if (num2 === 0) {
+    return Error(`${num2} can't divide by zero. Please, try again.`);
+  }
+
   let result = num1 % num2;
   if (result === 0) {
     result = num2;
   } else {
     num1 = num2;
     num2 = result;
-    result = getGreaterCommonDivider(num1, num2);
+    result = getGCD(num1, num2);
   }
   return result;
 };
@@ -21,12 +28,12 @@ const askDivisor = () => {
   const taskNumber = () => {
     const randomNumber1 = getRandomNumber(2, 100);
     const randomNumber2 = getRandomNumber(2, 100);
-    const randomTask = `${randomNumber1} ${randomNumber2}`;
+    const task = `${randomNumber1} ${randomNumber2}`;
     const number1 = (randomNumber1 > randomNumber2) ? randomNumber1 : randomNumber2;
     const number2 = (randomNumber1 > randomNumber2) ? randomNumber2 : randomNumber1;
 
-    const answer = String(getGreaterCommonDivider(number1, number2));
-    return [randomTask, answer];
+    const correctAnswer = String(getGCD(number1, number2));
+    return [task, correctAnswer];
   };
 
   runGame(question, taskNumber);
